34834ed4bcdbfcbc7d620abfde1907fb6bb39087
Liam Bradshaw <otherofice@gmail.com>
2016-03-13 18:58:17 +0000

    Commented out time code logs

--- ProcessSongs.py
+++ ProcessSongs.py
@@ -8,7 +8,7 @@ def GetSongList(desciption):
         if __lineContainsTimeCode(lineOfDescription):
             for part in lineList:
                 if re.search("[0-9]:?[0-9][0-9]",part) != None:
-                    print(part)
+                    #print(part)
                     time_codes = part.split(sep = ":")
                     time_codes = list(reversed(time_codes))
                     for time_code in range(len(time_codes)):
@@ -16,10 +16,10 @@ def GetSongList(desciption):
                         if time_code != 0:
                             temp_time *= (60 ** time_code)
 
-                        print(time_code, " = ", temp_time, " == ", time_codes[time_code])
-                        print("Times by:", (60 ** time_code))
+                        #print(time_code, " = ", temp_time, " == ", time_codes[time_code])
+                        #print("Times by:", (60 ** time_code))
                         song_time += temp_time
-                    print(song_time)
+                    #print(song_time)
                     break
 
                 if re.search("[a-z]",part) != None:
