61910efdc41fd9395ad1ed1cc2ebe85e45a5f7a5
Liam Bradshaw <otherofice@gmail.com>
2016-03-13 19:07:11 +0000

    Added a windows install script

---
+++ install.bat
@@ -0,0 +1,11 @@
+@echo off
+python -m pip install pydub
+python -m pip install html2text
+python -m pip install bs4
+python -m pip install youtube-dl
+echo ##################################################################
+echo ##################################################################
+echo Installed python libs
+echo DONE!
+echo p.s. Make sure to download ffpeg and add it to your env variables
+pause
